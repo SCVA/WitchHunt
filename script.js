1db32dc53f30cfc855d124265e30b34e48c8833b
SCVA <scva97@hotmail.com>
2017-05-22 23:06:36 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
 var x = 350;
 var y = 250;
 var duracion = 1000;
-var rotacion = 0;
-var msg = 0;
-var ast = [[aleatorio(700),aleatorio(500)],
-		   [aleatorio(700),aleatorio(500)],
-		   [aleatorio(700),aleatorio(500)],
-		   [aleatorio(700),aleatorio(500)],
-		   [aleatorio(700),aleatorio(500)],
-		   [aleatorio(700),aleatorio(500)],
-		   [aleatorio(700),aleatorio(500)],
-		   [aleatorio(700),aleatorio(500)],
-		   [aleatorio(700),aleatorio(500)],
-		   [aleatorio(700),aleatorio(500)]];
 
 $(document).ready(inicio);
-//$(document).click(capturaMouse);
-
 
 function inicio(){
 	var lienzo = $("#lienzo")[0];
@@ -26,15 +12,20 @@ function inicio(){
 	buffer.height = lienzo.height;
 	contextoBuffer = buffer.getContext("2d");
 	contextoBuffer.fillStyle = "#ffffff"; 
-	contextoBuffer.clearRect(0,0,700,500);
+	contextoBuffer.clearRect(0,0,1000,800);
 	contextoBuffer.font = "bold 50px sans-serif";
-	contextoBuffer.fillText("Hola!", 180, 200);
+	contextoBuffer.fillText("Witch Hunt", 220, 240);
 	contextoBuffer.fillStyle = "#ff0000"; 
 	contextoBuffer.font = "bold 30px sans-serif";
-	contexto.clearRect(0,0,700,500);
+	contexto.clearRect(0,0,1000,800);
 	contexto.drawImage(buffer, 0, 0);
 	$('#brisa')[0].play();
-	$("button").click(function(){	
+	$("#iniciar").click(function(){	
+		contexto.clearRect(0,0,1000,800);
+		contexto.drawImage(brujaP, x, 0);
+		//cursor: url(imagenes/mira.png), url(cursor.cur), pointer;
+		//cursor: url(imagenes/mira.png), pointer;
+		//$("#bruja").
 		x = 350;
 		y = 250;	
 		duracion = 1000;
@@ -43,55 +34,11 @@ function inicio(){
 	$("#recomendaciones").load("recomendaciones.html");
 }
 
-function aleatorio(tope){
-	return Math.floor((Math.random() * tope) + 1);
-} 
-
-function capturaMouse(event){
-	//alert(event.which);
-	$(this).css("background-color", "lightblue");
-}
-
-function Nave(){
-	this.img = [$("#bruja")[0],$("#explosion")[0]];
-	this.msgs = ["verifca el frente", "cuidado al lado", "mejor moverse", 
-	             "ojo alrededor", "cuidado con el asteroide", "evita los asteroides",
-	             "acelera","no puedes colisionar mas","debes mantener la vida",
-	             "atiende los controles"];
-	
-	this.dibujar = function(ctx,i){
-		var img = this.img[i];
-		ctx.drawImage(img, x, y);
-		ctx.save();
-		ctx.fillStyle = "#aaaaff";
-		ctx.font = "10px sans-serif";
-		ctx.fillText("pos x: "+ x + " pos y: " + y, x - 25, y + 50);
-		ctx.fillStyle = "#ff8888";
-		ctx.fillText(this.msgs[msg], x - 25, y + 60);
-		ctx.restore();
-	}
-	
-	this.colision = function(xx,yy){
-		var distancia=0;
-		distancia=Math.sqrt( Math.pow( (xx-x), 2)+Math.pow( (yy-y),2));
-		if(distancia>40)
-		   return false;
-		else
-		   return true;	
-	}
-}
-
-function Asteroid(){
-	this.img = $("#bruja")[0];			
-	this.dibujar = function(ctx, x1, y1){
-		var img = this.img;
-		ctx.save();
-		ctx.translate(x1,y1);
-		ctx.rotate(rotacion*Math.PI/180);
-		ctx.drawImage(img,-img.width/2,-img.height/2);
-		ctx.restore();
-	}
-}
+var x = 200;
+		var y = 300;
+		var x2 = 80;
+		var y2 = 80;
+		
 
 function run(){ 
 	var lienzo = $("#lienzo")[0];
@@ -103,7 +50,9 @@ function run(){
 	contextoBuffer.fillStyle = "#ffffff"; 
 	if(duracion >= 0){  		
 		duracion--;
-		if(duracion % 50 == 0)
+		dibujar(x,y,x2,y2);
+		click();
+		/*if(duracion % 50 == 0)
 			//msg = aleatorio(9);
 		var objnave = new Nave();
 		var objasteroid = [new Asteroid(),new Asteroid(),new Asteroid(),
@@ -113,19 +62,19 @@ function run(){
 		contextoBuffer.clearRect(0,0,700,500);
 
 		contextoBuffer.font = "bold 25px sans-serif";
-		contextoBuffer.fillText("vida = "+vida, 25, 25);
+		contextoBuffer.fillText("tiempo = "+duracion, 25, 25);
 		contextoBuffer.fillText("puntos = "+parseInt(duracion/10), 250, 25);
-		objnave.dibujar(contextoBuffer,0);
+		//objnave.dibujar(contextoBuffer,0);
 		rotacion -= 5;
 		for(i=0;i<10;i++){
 			
 			objasteroid[i].dibujar(contextoBuffer,ast[i][0],ast[i][1]);
-			if(objnave.colision(ast[i][0],ast[i][1])){
+			/*if(objnave.colision(ast[i][0],ast[i][1])){
 				vida -=1;
 				objnave.dibujar(contextoBuffer,1);
 				$('#explode')[0].play();
-			}
-			ast[i][0]-=5;
+			}*/
+			/*ast[i][0]-=5;
 			ast[i][1]+=3;
 			ast[i][0] = (700 + ast[i][0])%700;
 			ast[i][1] = (500 + ast[i][1])%500;
@@ -133,7 +82,7 @@ function run(){
 		contexto.clearRect(0,0,700,500);
 		contexto.drawImage(buffer, 0, 0);
 		setTimeout("run()",20);
-		$("button").css("display","none");
+		$("button").css("display","none");*/
 	}else{
 		$('#brisa')[0].pause();
 		
@@ -149,4 +98,44 @@ function run(){
 	}
 }
 
+function click(){
+	canvas = $("#lienzo")[0];
+	ctx = canvas.getContext("2d");
+	//Añadimos un addEventListener al canvas, para reconocer el click
+	canvas.addEventListener("click",   
+	//Una vez se haya clickado se activará la siguiente función
+	function(e){
+	/** Las siguientes 2 líneas lo que hacen és saber en que 
+		*parte del canvas se ha clickado. Si ha clickado dentro
+		* del cuadrado se activará mover().
+		* La operación que utilizamos es restar la parte izquierda que sobra
+		* des del canvas asta el explorador (cancas.offsetLeft)
+		* con el click (e.clientY)
+		*/
+		if(e.clientX-canvas.offsetLeft > 200 && e.clientX-canvas.offsetLeft < 280){  
+			if(e.clientY-canvas.offsetTop > 300 && e.clientY-canvas.offsetTop < 380){  
+				//si ha clickado dentro del cuadro verde
+			mover();   
+			} 
+		}
+	}
+,false);}
 
+function mover(){
+	if(y>150){
+		y--;
+		//es la encargada de llamar cada 10 milesimas a la funcion mover()
+		setTimeout(mover,10);
+	}
+	//limpiar();
+	dibujar(x,y,x2,y2);
+	alert("Hello! I am an alert box!!");
+}
+
+function dibujar(x,y,x2,y2){
+	var lienzo = $("#lienzo")[0];
+	var ctx = lienzo.getContext("2d");
+	ctx.fillStyle = "green";
+	ctx.fillRect(x, y, x2, y2);
+	ctx.fill();
+}
